3087198784f3bcb21ba62ae9645abbbf8ae7bfc0
dhruveeel <144720559+dhruveeel@users.noreply.github.com>
2024-03-07 15:12:26 -0700

    listview from firebase 1

--- ScanDal/app/src/main/java/com/example/scandal/MainActivity.java
+++ ScanDal/app/src/main/java/com/example/scandal/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         toQrScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent_profile = new Intent(MainActivity.this, QRActivity.class);
+                Intent intent_profile = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent_profile);
             }
         });
