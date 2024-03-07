6035c5cb1463a85a0dd90323ae0b6caf5e699d2c
dhruveeel <144720559+dhruveeel@users.noreply.github.com>
2024-03-07 04:35:11 -0700

    javadocs started

--- ScanDal/app/src/main/java/com/example/scandal/EventActivity.java
+++ ScanDal/app/src/main/java/com/example/scandal/EventActivity.java
@@ -1,17 +1,12 @@
 package com.example.scandal;
-import static android.app.PendingIntent.getActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Base64;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -26,120 +21,93 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-/**
- * This is an activity involving
- * Adding new events' information to the database
- */
 
+/**
+ * Activity for creating and saving event details, including the event name,
+ * description, and an associated poster image, into Firebase Firestore.
+ */
 public class EventActivity extends AppCompatActivity {
-    private String name;
-    private String description;
+    // URI of the selected poster image
     private Uri imageUri;
+    // ImageView to display the selected poster
     ImageView poster;
+    // EditText for event name input
     private EditText editEventName;
+    // EditText for event description input
     private EditText editEventDescription;
+    // Button to trigger event data saving
     AppCompatButton generateEventButton;
+    // Button to upload a poster image
     AppCompatButton uploadPosterButton;
+    // Button to delete the selected poster image
     AppCompatButton deletePosterButton;
+    // Firebase Firestore instance for database operations
     private FirebaseFirestore db;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event_page);
 
-        poster = findViewById(R.id.imageView3);
-        editEventName = findViewById(R.id.editTextEventName);
-        editEventDescription = findViewById(R.id.editTextEventDescription);
-        generateEventButton = findViewById(R.id.buttonSave_CreateEventPage);
-        uploadPosterButton = findViewById(R.id.editPosterButton);
-        deletePosterButton = findViewById(R.id.deletePosterButton);
+        // Initialize UI components and Firestore
+        initializeUIComponents();
 
-        db = FirebaseFirestore.getInstance();
-        // Generate Event Button
-        generateEventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveEventData();
-            }
-        });
-        // Image Edit Button
-        uploadPosterButton.setOnClickListener(new View.OnClickListener() {
-            /**
-             * The api is from https://github.com/Dhaval2404/ImagePicker
-             */
-            @Override
-            public void onClick(View view) {
-                ImagePicker.with(EventActivity.this)
-                        .crop() // Crop image(Optional)
-                        .compress(1024) // Final image size will be less than 1 MB(Optional)
-                        .maxResultSize(1080, 1080) //Final image resolution will be less than 1080 x 10800
-                        .start();
-            }
-
-        });
-        // Image Delete Button
-        deletePosterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                poster.setImageResource(R.drawable.sample_poster); // Show the default profile picture after deletion
-            }
-        });
+        // Setup listeners for button actions
+        setupButtonListeners();
     }
-    // Method to update the image view
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == ImagePicker.REQUEST_CODE) {
-                // Convert image URI to string
-                imageUri = data.getData();
-                String imageString = convertImageUriToString(imageUri);
-                if (imageString != null) {
-                    // Convert image string to bitmap
-                    Bitmap bitmap = convertImageStringToBitmap(imageString);
-                    if (bitmap != null) {
-                        // Set image view to display the bitmap
-                        poster.setImageBitmap(bitmap);
-                    } else {
-                        Toast.makeText(this, "Failed to convert image string to bitmap", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(this, "Failed to convert image to string", Toast.LENGTH_SHORT).show();
-                }
+        // Process result from Image Picker
+        handleImagePickerResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Saves event details to Firebase Firestore. Includes the event name, description,
+     * and a Base64 encoded string of the poster image.
+     */
+    private void saveEventData() {
+        String name = editEventName.getText().toString().trim();
+        String description = editEventDescription.getText().toString().trim();
+
+        if (!name.isEmpty() && !description.isEmpty() && imageUri != null) {
+            String imageString = convertImageUriToString(imageUri);
+            if (imageString != null) {
+                Map<String, Object> event = new HashMap<>();
+                event.put("name", name);
+                event.put("description", description);
+                event.put("posterImage", imageString); // Add the image string to the event map
+
+                // Save event to Firestore
+                db.collection("events")
+                        .add(event)
+                        .addOnSuccessListener(documentReference -> Toast.makeText(getApplicationContext(), "Event created successfully", Toast.LENGTH_SHORT).show())
+                        .addOnFailureListener(e -> Toast.makeText(getApplicationContext(), "Failed to create event", Toast.LENGTH_SHORT).show());
+            } else {
+                Toast.makeText(getApplicationContext(), "Failed to convert image to string", Toast.LENGTH_SHORT).show();
             }
-        } else if (resultCode == ImagePicker.RESULT_ERROR) {
-            Toast.makeText(this, ImagePicker.Companion.getError(data), Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Please enter event name and description, and upload a poster", Toast.LENGTH_SHORT).show();
         }
     }
 
-    // Method to convert image string to bitmap
-    private Bitmap convertImageStringToBitmap(String imageString) {
-        try {
-            byte[] decodedString = Base64.decode(imageString, Base64.DEFAULT);
-            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    // Method to convert image URI to string
+    /**
+     * Converts the image located at the provided Uri to a Base64 encoded string.
+     * @param imageUri The Uri of the image to be converted.
+     * @return A Base64 encoded string representation of the image, or null if conversion fails.
+     */
     private String convertImageUriToString(Uri imageUri) {
         try {
             InputStream inputStream = getContentResolver().openInputStream(imageUri);
             Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
             byte[] byteArray = byteArrayOutputStream.toByteArray();
             return Base64.encodeToString(byteArray, Base64.DEFAULT);
         } catch (Exception e) {
@@ -147,40 +115,50 @@ public class EventActivity extends AppCompatActivity {
             return null;
         }
     }
-    private void saveEventData() {
-        name = editEventName.getText().toString();
-        description = editEventDescription.getText().toString();
 
-        if (!name.isEmpty() && !description.isEmpty()) {
-            // Convert image URI to string
-            String imageString = convertImageUriToString(imageUri);
+    /**
+     * Initializes UI components and configures the Firebase Firestore instance.
+     */
+    private void initializeUIComponents() {
+        poster = findViewById(R.id.imageView3);
+        editEventName = findViewById(R.id.editTextEventName);
+        editEventDescription = findViewById(R.id.editTextEventDescription);
+        generateEventButton = findViewById(R.id.buttonSave_CreateEventPage);
+        uploadPosterButton = findViewById(R.id.editPosterButton);
+        deletePosterButton = findViewById(R.id.deletePosterButton);
+        db = FirebaseFirestore.getInstance();
+    }
 
-            if (imageString != null) {
-                Map<String, Object> event = new HashMap<>();
-                event.put("name", name);
-                event.put("description", description);
+    /**
+     * Sets up listeners for various button actions within the activity.
+     */
+    private void setupButtonListeners() {
+        generateEventButton.setOnClickListener(v -> saveEventData());
 
-                // Add a new document with a generated ID
-                db.collection("events")
-                        .add(event)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Toast.makeText(getApplicationContext(), "Event created successfully", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(getApplicationContext(), "Failed to create event", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            } else {
-                Toast.makeText(getApplicationContext(), "Failed to convert image to string", Toast.LENGTH_SHORT).show();
-            }
+        uploadPosterButton.setOnClickListener(view -> ImagePicker.with(EventActivity.this)
+                .crop() // Optional image cropping
+                .compress(1024) // Compress image size
+                .maxResultSize(1080, 1080) // Max resolution
+                .start());
+
+        deletePosterButton.setOnClickListener(view -> poster.setImageResource(R.drawable.sample_poster)); // Reset to default poster
+    }
+
+    /**
+     * Handles the result returned from the image picker activity. Sets the selected
+     * image as the poster's ImageView content if successful.
+     * @param requestCode The request code passed in startActivityForResult().
+     * @param resultCode The result code returned by the child activity.
+     * @param data An Intent that carries the result data.
+     */
+    private void handleImagePickerResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == ImagePicker.REQUEST_CODE && data != null) {
+            imageUri = data.getData();
+            poster.setImageURI(imageUri); // Directly update the ImageView with the image Uri
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.Companion.getError(data), Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(getApplicationContext(), "Please enter event name and description", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
 }
-
