5dd6afe28b4f074ba76d7f2553e00c140b8e9a73
Ethan Towsley <etowsley@ualberta.ca>
2024-03-08 14:21:09 -0700

    added javadoc to CustomImageAdapter

--- ScanDal/app/src/main/java/com/example/scandal/CustomImageAdapter.java
+++ ScanDal/app/src/main/java/com/example/scandal/CustomImageAdapter.java
@@ -12,17 +12,37 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.List;
-
+/**
+ * CustomImageAdapter class extends ArrayAdapter to create a custom adapter for displaying Bitmap images in a ListView.
+ */
 public class CustomImageAdapter extends ArrayAdapter<Bitmap> {
+    /**
+     * Context reference for the adapter
+     */
     private Context mContext;
+    /**
+     * List of Bitmap images to be displayed
+     */
     private List<Bitmap> mImages;
-
+    /**
+     * Constructor for CustomImageAdapter.
+     *
+     * @param context The context of the activity or fragment.
+     * @param images  The list of Bitmap images to be displayed.
+     */
     public CustomImageAdapter(Context context, List<Bitmap> images) {
         super(context, 0, images);
         mContext = context;
         mImages = images;
     }
-
+    /**
+     * Overrides the getView method of ArrayAdapter to customize the appearance of each item in the ListView.
+     *
+     * @param position    The position of the item within the adapter's data set.
+     * @param convertView The old view to reuse, if possible.
+     * @param parent      The parent that this view will eventually be attached to.
+     * @return The View corresponding to the data at the specified position.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
