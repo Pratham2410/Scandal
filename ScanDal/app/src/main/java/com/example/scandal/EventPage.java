cf3178b0bf99408006e86ee160ccbcdd99386163
unknown <harrisonkpeebles@gmail.com>
2024-03-07 23:51:04 -0700

    Comments added

--- ScanDal/app/src/main/java/com/example/scandal/EventPage.java
+++ ScanDal/app/src/main/java/com/example/scandal/EventPage.java
@@ -11,11 +11,26 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * EventPage is for displaying the event page and handling it's
+ * respective actions.
+ */
+
 public class EventPage extends AppCompatActivity {
+
     ImageView poster;
     FrameLayout back;
     TextView eventName;
     TextView eventDescription;
+
+    /**
+     * takes care of the activities required functionality utilizing the
+     * view_event_page xml
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
