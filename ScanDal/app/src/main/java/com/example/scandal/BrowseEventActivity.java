a94f32db1d5386476fc8a0d4c38153f4a005ae9d
hkpeebles <144843146+hkpeebles@users.noreply.github.com>
2024-03-08 16:24:11 +0000

    Update BrowseEventActivity.java
    i think a few lines were mistakenly deleted

--- ScanDal/app/src/main/java/com/example/scandal/BrowseEventActivity.java
+++ ScanDal/app/src/main/java/com/example/scandal/BrowseEventActivity.java
@@ -35,7 +35,22 @@ public class BrowseEventActivity extends AppCompatActivity {
      * Firebase Firestore instance for database operations.
      */
     FirebaseFirestore db;
+/**
+     * Called when the activity is starting. This is where most initialization should go:
+     * calling setContentView(int) to inflate the activity's UI, initializing objects, etc.
+     * and setting up listeners for the layout page
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down
+     *                           then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     *                           Note: Otherwise, it is null.
+     */
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse_events_page);
 
+        buttonBack_BrowseEventsPage = findViewById(R.id.buttonBack_BrowseEventsPage);
+        db = FirebaseFirestore.getInstance();
+        eventsList = findViewById(R.id.listView_BrowseEventPage);
        /**
        * Provides functionality for event list
        */
