b29981aabdcac4b0b2cfa2856cf5af028c083929
dhruveeel <144720559+dhruveeel@users.noreply.github.com>
2024-03-20 00:35:07 -0600

    test notification

---
+++ ScanDal/app/src/main/java/com/example/scandal/MyFirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package com.example.scandal;
+
+import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // You can save the token into your server database here.
+    }
+}
+
