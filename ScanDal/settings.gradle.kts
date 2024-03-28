686472f27d0e39145ceba5a3d9e2ec1d709e4cb4
Ethan <156837550+ethandt24@users.noreply.github.com>
2024-03-28 12:32:31 -0600

    Update settings.gradle.kts

--- ScanDal/settings.gradle.kts
+++ ScanDal/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.1.3" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 dependencyResolutionManagement {
