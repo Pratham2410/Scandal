0e8ad207c5cb5df3ab169da9d0fe6d2fcc4690e0
Ethan <ethandt24@gmail.com>
2024-04-04 15:24:15 -0600

    Changed variable names in NewEventActivity.java

--- ScanDal/settings.gradle.kts
+++ ScanDal/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 dependencyResolutionManagement {
